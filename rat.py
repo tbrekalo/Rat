656f721e3402464ba7da95225d1cb7c44e077be9
tbreklo <brekalo.tvrtko@gmail.com>
2020-04-04 10:57:09 +0200

    Match grouping per reference. PAF grouped output.

--- rat.py
+++ rat.py
@@ -2,17 +2,64 @@ import os
 import re
 import sys
 import argparse
+import operator
+
+from collections import namedtuple
 
 import mappy as mp
 
-import util
+from util import check_path, create_clean_dir
 
-# Dummy: Listeria_monocytogenes_complete_genome.fasta
+
+class _PafHolder():
+    ''' List wrapper for PAF alignments '''
+
+    PafScored = namedtuple('PafScored', 'n_matches str')
+
+    def __init__(self, ref_name, ovlp_dir):
+        self.ref_name = ref_name
+        self.out_dir = create_clean_dir(ovlp_dir, ref_name)
+
+        self.pafs = {}
+
+
+    def __sort_all_pafs(self, reverse=False):
+        ''' Inplae sorting of PAF based on match score '''
+
+        for pafs in self.pafs.values():
+            pafs.sort(reverse=not reverse, key=operator.itemgetter(0))
+
+
+    def add_paf(self, q_name, n_matches, paf_str):
+        if q_name not in self.pafs:
+            self.pafs[q_name] = []
+        
+        self.pafs[q_name].append(_PafHolder.PafScored(n_matches, paf_str))
+
+ 
+    def dump(self):
+        self.__sort_all_pafs()
+        for contig, pafs in self.pafs.items():
+            file_path = os.path.join(self.out_dir, contig + '.paf')
+
+            print(file_path, file=sys.stderr)
+            with open(file_path, 'w+') as dist:
+                for paf in pafs:
+                    dist.write(paf.str + '\n')
 
 
 class Rat:
     ''' Slices metegenomic accembly into contings matching references.
     '''
+
+    # Defaults
+    __THRESHOLD = int(1e4)
+    __N_THREADS = 3
+    __BEST_N = 10
+
+    @staticmethod
+    def __gen_paf():
+        pass
 
     @staticmethod
     def __init_parser():
@@ -25,12 +72,12 @@ class Rat:
                     Format: rat [options] <assembly.(fasta|fa)> <ref_dir> <out_dir>''',
             formatter_class=argparse.RawTextHelpFormatter)
 
-        parser.add_argument('--threshold', type=int, dest='threshold', action='store', default=int(1e4),
-                            help='Overlaps with lower length are discarded. Default: 10 000')
-        parser.add_argument('--top', type=int, dest='top', action='store', default=5,
-                            help='Number of longest overlaps taken as valid matches. Default=5')
-        parser.add_argument('-t', type=int, dest='n_threads', action='store', default=1,
-                            help='Number of threads used by minimap. Default=1, Max=3')
+        parser.add_argument('--threshold', type=int, dest='threshold', action='store', default=Rat.__THRESHOLD,
+                            help='Overlaps with lower length are discarded. Default: {}'.format(Rat.__THRESHOLD))
+        parser.add_argument('--best_n', type=int, dest='best_n', action='store', default=Rat.__BEST_N,
+                            help='Number of longest overlaps taken as valid matches. Default={}'.format(Rat.__BEST_N))
+        parser.add_argument('-t', type=int, dest='n_threads', action='store', default=Rat.__N_THREADS,
+                            help='Number of threads used by minimap. Default=3')
 
         parser.add_argument('assembly_fasta', nargs=1, type=str)
         parser.add_argument('ref_dir', nargs=1, type=str)
@@ -38,28 +85,73 @@ class Rat:
 
         return parser
 
-
     def __parse_args(self, args_raw):
         ''' Parses args from list or command line 
-            and initializes Rat config, parameters ''' 
+            and initializes Rat config, parameters '''
 
         args = self.__parser.parse_args(args_raw)
 
-        self.__conf = {
-            'n_threads': args.n_threads,
-            'threshold': args.threshold,
-            'top': args.top,
-        }
+        self.__n_threads = args.n_threads
+        self.__threshold = args.threshold
+        self.__best_n = args.best_n
 
+        self.__assembly_fasta = check_path(args.assembly_fasta[0])
+        self.__ref_dir = check_path(args.ref_dir[0])
+        self.__out_dir = check_path(args.out_dir[0])
 
+    def __create_aligner(self, seq_path):
+        ''' Creates a minimap2 aligner via mappy binding
 
-        self.__assembly_fasta = args.assembly_fasta[0]
-        self.__ref_dir = args.ref_dir[0]
-        self.__out_dir = args.out_dir[0]
+            Params:
+                seq_path: Path to reference sequence
+
+            Returns:
+                mappy.Aligner()
+
+            Raises:
+                Exception if fail to create aligner
+
+        '''
+
+        aligner = mp.Aligner(
+            fn_idx_in=seq_path,
+            n_threads=self.__n_threads,
+            preset=self.__preset,
+            best_n=self.__best_n,
+
+            # TODO: Extra flags
+        )
+
+        if not aligner:
+            raise Exception('Filed to create index for{}'.format(
+                self.__assembly_fasta))
+
+        return aligner
+
+    def __init_asm(self):
+        ''' Loads contings from fasta assembly into a dictionary
+
+            Returns:
+                asm: {seq_id: seq} dictionary
+        '''
+
+        asm = {}
+        for name, seq, _ in mp.fastx_read(self.__assembly_fasta):
+            asm[name] = seq
+
+        return asm
+
+    def __init_dirs(self):
+        ''' Creates directories for PAF output and graphics '''
+
+        ovlp_dir = create_clean_dir(self.__out_dir, 'rat_overlaps', True)
+        plot_dir = create_clean_dir(self.__out_dir, 'rat_plots', True)
+
+        return ovlp_dir, plot_dir
 
     def __init__(self, args=None):
         ''' Initializes Rat and parses arguments
-        
+
             Args:
                 args: List of arguments to be parsed.
                         Default: None, propagates to command line.
@@ -69,10 +161,52 @@ class Rat:
         self.__parser = self.__init_parser()
         self.__parse_args(args)
 
+        self.__asm = self.__init_asm()
+
+        # Asm to ref mapping
+        self.__preset = 'asm20'
+
+        ovlp_dir, plot_dir = self.__init_dirs()
+
+        self.__ovlp_dir = ovlp_dir
+        self.__plot_dir = plot_dir
+
+    def __map_to(self, ref_name, ref_path):
+        ''' Maps assembly to reference sequence.
+
+            Dumps PAFs into specified folder via cmd arguments
+
+            Args:
+                ref_name: reference genome name
+                ref_path: path to reference fasta file
+
+            Returns:
+                pafs: _PafHolder with all conting matches
+        '''
+
+        def fmt_paf(q_name, q_len, align):
+            return '{} {} {}'.format(q_name, q_len, str(align).rsplit('cg:')[0])
+
+        aligner = self.__create_aligner(ref_path)
+        pafs = _PafHolder(ref_name, self.__ovlp_dir)
+
+        for q_name, q_str in self.__asm.items():
+            for hit in aligner.map(q_str):
+                paf_str = fmt_paf(q_name, len(q_str), hit)
+                pafs.add_paf(q_name, hit.mlen, paf_str)
+
+        return pafs
 
     def run(self):
         fasta_re = r'\S+.(fasta|fa)'
-        for filename in os.listdir(self.__ref_dir):
-            if not re.match(fasta_re, filename):
-                print('[WARNING] Skipping {}.'.format(filename), file=sys.stderr)
-            print(filename)
+        def get_seq_path(seq): return os.path.join(self.__ref_dir, seq)
+
+        for file_name in os.listdir(self.__ref_dir):
+            ref_path = get_seq_path(file_name)
+            ref_name = file_name.split('.')[0]
+            if not re.match(fasta_re, ref_path):
+                print('[WARNING] Skipping {}.'.format(
+                    ref_name), file=sys.stderr)
+
+            pafs = self.__map_to(ref_name, ref_path)
+            pafs.dump()
--- util.py
+++ util.py
@@ -1,14 +1,63 @@
-''' Set of utility functions used across the module '''
+''' Utility functions used across Meta-Rat '''
 
 import os
+import sys
+import shutil
 
-def __check_paths( paths):
-    ''' Checks if supplied path arguments are valid.
+
+def check_path(file_path):
+    ''' Checks whether file/dir exists.
+        Returns path string representation.
 
         Raises:
-            FileNotFoundError
+            FileNotFoundError '''
+
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(file_path)
+
+    return file_path
+
+
+def clean_dir(dir_path):
+    ''' Removes all files in a folder and unlinks sym-links
+
+        Args:
+            dir_path: path to target directory 
+
     '''
 
-    for path in paths:
-        if not os.path.exists(paths):
-            raise FileNotFoundError(path)
+    def gen_file_path(filename): return os.path.join(dir_path, filename)
+
+    for file_path in map(gen_file_path, os.listdir(dir_path)):
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('[ERROR] Failed to delete {}\n Message: {}'.foramt(
+                file_path, e), file=sys.stderr)
+
+    pass
+
+
+def create_clean_dir(parent_path, name, parent_checked=False):
+    ''' Creates a new directory in parent directory
+        or remove all files directory already exists 
+        
+        Returns:
+            dir_path: str path to clean directory
+    '''
+
+    if not parent_checked:
+        check_path(parent_path)    
+    
+    dir_path = os.path.join(parent_path, name)
+    
+    if os.path.exists(dir_path):
+        clean_dir(dir_path)
+    else:
+        os.mkdir(dir_path)
+
+    return dir_path
+
